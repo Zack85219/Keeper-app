3a82e43d03f90b14310ba9f51b878b65ebb0f59c
zach <j33241796@gmail.com>
2022-11-19 18:02:41 +0800

    Updates

--- src/components/BackgroundImg.jsx
+++
@@ -1,5 +0,0 @@
-function BackgroundImg(props) {
-  return <div className="back-image">{props.backImg}</div>;
-}
-
-export default BackgroundImg;
--- src/components/DeleteArea.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function DeleteArea(props) {
-  return <div className="dArea">{props.noteDeleted}</div>;
-}
-
-export default DeleteArea;
